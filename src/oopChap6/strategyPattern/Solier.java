e321d41e233368d74b3265bb0e71c1b1335ae08a
YH3041 <mmj2rv9@gmail.com>
2020-12-26 23:30:22 +0900

    OOP Practice Design Pattern

--- src/oopChap6/strategyPattern/Solier.java
+++ src/oopChap6/strategyPattern/Solier.java
@@ -1,9 +1,17 @@
 package oopChap6.strategyPattern;
 
 public class Solier {
-    void runContext(Strategy strategy) {
+    void runContext(String weaponSound) {
         System.out.println("전투 시작");
-        strategy.runStrategy();
+        executeWeapon(weaponSound).runStrategy();
         System.out.println("전투 종료");
+    }
+
+    private Strategy executeWeapon(final String weaponSound) {
+        return new Strategy() {
+            public void runStrategy() {
+                System.out.println(weaponSound);
+            }
+        };
     }
 }
